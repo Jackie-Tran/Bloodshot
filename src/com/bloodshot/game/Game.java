e004ea99cf775de15653ea16461815e840a68564
Jackie-Tran <jtran2009a@gmail.com>
2019-09-11 18:27:38 -0400

    finished Game class

--- src/com/bloodshot/game/Game.java
+++ src/com/bloodshot/game/Game.java
@@ -1,7 +1,115 @@
 package com.bloodshot.game;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JOptionPane;
 
 public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+
+    // Variables for the window
+    public static final int WIDTH = 1024;
+    public static final int HEIGHT = WIDTH / 16 * 9;
+    public static final String TITLE = "Bloodshot v0.01";
+    public static int FPS;
+
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+
+    // Variables for the game
+    private boolean running = false;
+    private Thread game;
+
+    public Game() {
+	new Window(TITLE, WIDTH, HEIGHT, this);
+    }
+
+    public synchronized void start() {
+	if (running)
+	    return;
+	running = true;
+	game = new Thread(this);
+
+	game.start();
+    }
+
+    public synchronized void stop() {
+	running = false;
+	if (game != null) {
+	    try {
+		game.join();
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    public void run() {
+	long lastTime = System.nanoTime();
+	double amountOfTicks = 60.0;
+	double ns = 1000000000 / amountOfTicks;
+	double delta = 0;
+	long timer = System.currentTimeMillis();
+	int updates = 0;
+	int frames = 0;
+
+	while (running) {
+	    long now = System.nanoTime();
+	    delta += (now - lastTime) / ns;
+	    lastTime = now;
+	    while (delta >= 1) {
+		update();
+		updates++;
+		delta--;
+	    }
+	    render();
+	    frames++;
+
+	    if (System.currentTimeMillis() - timer > 1000) {
+		timer += 1000;
+		FPS = frames;
+		System.out.println("FPS: " + frames + "\tUPDATES: " + updates);
+
+		frames = 0;
+		updates = 0;
+	    }
+	}
+    }
+
+    public void update() {
+	
+    }
+
+    public void render() {
+	BufferStrategy bs = this.getBufferStrategy();
+
+	if (bs == null) {
+	    this.createBufferStrategy(3);
+	    return;
+	}
+
+	Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+	// ///////////////////////////////
+	// DRAW HERE
+	// ///////////////////////////////
+	g.dispose();
+	bs.show();
+    }
+
+    public static void main(String args[]) {
+	Game game = new Game();
+	System.out.println("WIDTH: " + game.getWidth() + " HEIGHT: " + game.getHeight());
+	game.start();
+	game.requestFocus();
+    }
 
 }
--- src/com/bloodshot/game/Window.java
+++ src/com/bloodshot/game/Window.java
@@ -3,19 +3,17 @@ package com.bloodshot.game;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-public class Window extends JFrame{
-  public Window(String title, int width, int height) {
-    setName(title);
-    
-    Dimension dimension = new Dimension(width, height);
-    setPreferredSize(dimension);
-    setMinimumSize(dimension);
-    setMaximumSize(dimension);
-    setVisible(true);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
-  
-  public static void main(String args[]) {
-    Window game = new Window("Bloodshot", 900, 600);
-  }
+public class Window extends JFrame {
+    public Window(String title, int width, int height, Game game) {
+	Dimension dimension = new Dimension(width, height);
+	setTitle(title);
+	setPreferredSize(dimension);
+	setMinimumSize(dimension);
+	setMaximumSize(dimension);
+	add(game);
+	setResizable(false);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setLocationRelativeTo(null);
+	setVisible(true);
+    }
 }
