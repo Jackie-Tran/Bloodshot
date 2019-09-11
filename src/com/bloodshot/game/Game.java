9ec8e3ea8384d768c921f61981ebd4f6db2196ac
H-Richard <r.hong@pm.me>
2019-09-11 18:48:49 -0400

    game updates

--- src/com/bloodshot/game/Game.java
+++ src/com/bloodshot/game/Game.java
@@ -3,110 +3,110 @@ package com.bloodshot.game;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-
-import javax.swing.JOptionPane;
+import com.bloodshot.objects.Tree;
 
 public class Game extends Canvas implements Runnable {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    // Variables for the window
-    public static final int WIDTH = 1024;
-    public static final int HEIGHT = WIDTH / 16 * 9;
-    public static final String TITLE = "Bloodshot v0.01";
-    public static int FPS;
+  // Variables for the window
+  public static final int WIDTH = 1024;
+  public static final int HEIGHT = WIDTH / 16 * 9;
+  public static final String TITLE = "Bloodshot v0.01";
+  public static int FPS;
 
-    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+  private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
 
-    // Variables for the game
-    private boolean running = false;
-    private Thread game;
+  // Variables for the game
+  private boolean running = false;
+  private Thread game;
 
-    public Game() {
-	new Window(TITLE, WIDTH, HEIGHT, this);
+  //TEMP
+  private Tree tree = new Tree(100, 100, 10, 10);
+  
+  public Game() {
+    new Window(TITLE, WIDTH, HEIGHT, this);
+  }
+
+  public synchronized void start() {
+    if (running)
+      return;
+    running = true;
+    game = new Thread(this);
+
+    game.start();
+  }
+
+  public synchronized void stop() {
+    running = false;
+    if (game != null) {
+      try {
+        game.join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void run() {
+    long lastTime = System.nanoTime();
+    double amountOfTicks = 60.0;
+    double ns = 1000000000 / amountOfTicks;
+    double delta = 0;
+    long timer = System.currentTimeMillis();
+    int updates = 0;
+    int frames = 0;
+
+    while (running) {
+      long now = System.nanoTime();
+      delta += (now - lastTime) / ns;
+      lastTime = now;
+      while (delta >= 1) {
+        update();
+        updates++;
+        delta--;
+      }
+      render();
+      frames++;
+
+      if (System.currentTimeMillis() - timer > 1000) {
+        timer += 1000;
+        FPS = frames;
+        System.out.println("FPS: " + frames + "\tUPDATES: " + updates);
+
+        frames = 0;
+        updates = 0;
+      }
+    }
+  }
+
+  public void update() {
+    tree.Update();
+  }
+
+  public void render() {
+    BufferStrategy bs = this.getBufferStrategy();
+
+    if (bs == null) {
+      this.createBufferStrategy(3);
+      return;
     }
 
-    public synchronized void start() {
-	if (running)
-	    return;
-	running = true;
-	game = new Thread(this);
+    Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+    g.setColor(Color.BLACK);
+    g.fillRect(0, 0, getWidth(), getHeight());
+    tree.Render(g);
+    g.dispose();
+    bs.show();
+  }
 
-	game.start();
-    }
-
-    public synchronized void stop() {
-	running = false;
-	if (game != null) {
-	    try {
-		game.join();
-	    } catch (InterruptedException e) {
-		e.printStackTrace();
-	    }
-	}
-    }
-
-    public void run() {
-	long lastTime = System.nanoTime();
-	double amountOfTicks = 60.0;
-	double ns = 1000000000 / amountOfTicks;
-	double delta = 0;
-	long timer = System.currentTimeMillis();
-	int updates = 0;
-	int frames = 0;
-
-	while (running) {
-	    long now = System.nanoTime();
-	    delta += (now - lastTime) / ns;
-	    lastTime = now;
-	    while (delta >= 1) {
-		update();
-		updates++;
-		delta--;
-	    }
-	    render();
-	    frames++;
-
-	    if (System.currentTimeMillis() - timer > 1000) {
-		timer += 1000;
-		FPS = frames;
-		System.out.println("FPS: " + frames + "\tUPDATES: " + updates);
-
-		frames = 0;
-		updates = 0;
-	    }
-	}
-    }
-
-    public void update() {
-	
-    }
-
-    public void render() {
-	BufferStrategy bs = this.getBufferStrategy();
-
-	if (bs == null) {
-	    this.createBufferStrategy(3);
-	    return;
-	}
-
-	Graphics2D g = (Graphics2D) bs.getDrawGraphics();
-	g.dispose();
-	bs.show();
-    }
-
-    public static void main(String args[]) {
-	Game game = new Game();
-	System.out.println("WIDTH: " + game.getWidth() + " HEIGHT: " + game.getHeight());
-	game.start();
-	game.requestFocus();
-    }
+  public static void main(String args[]) {
+    Game game = new Game();
+    System.out.println("WIDTH: " + game.getWidth() + " HEIGHT: " + game.getHeight());
+    game.start();
+    game.requestFocus();
+  }
 
 }
--- src/com/bloodshot/objects/Tree.java
+++ src/com/bloodshot/objects/Tree.java
@@ -1,5 +1,34 @@
 package com.bloodshot.objects;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Tree extends Object{
+  
+  private BufferedImage image;
+
+  public Tree(int x, int y, int width, int height) {
+    super(x, y, width, height);
+    try {
+      image = ImageIO.read(getClass().getResource("/Props/Trees/tree01.png"));
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void Update() {
+    // TODO Auto-generated method stub
+    x += 5;
+  }
+
+  @Override
+  public void Render(Graphics2D g) {
+    g.drawImage(image, null, x, y);
+  }
 
 }
